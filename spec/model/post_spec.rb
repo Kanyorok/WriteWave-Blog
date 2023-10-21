e003c0d5b42e8711357a1a997eae98ee3dc37482
Robert Kanyoro <107240977+Kanyorok@users.noreply.github.com>
2023-10-21 16:09:51 +0300

    Fix linter errors in the code

--- spec/model/post_spec.rb
+++ spec/model/post_spec.rb
@@ -14,9 +14,10 @@ RSpec.describe Post, type: :model do
   end
   it 'should be invalid post [maximum length for title is 250]' do
     post.Title = '
-    Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero,
-    written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance.
-    The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.'
+    Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of
+    Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics,
+    very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..",
+    comes from a line in section 1.10.32.'
     expect(post).to_not be_valid
   end
   it 'should have post counter to 1' do
