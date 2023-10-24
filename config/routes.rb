f28c15a2157ad03ef500ccaaf5de8304890e9686
Robert Kanyoro <107240977+Kanyorok@users.noreply.github.com>
2023-10-24 19:17:53 +0300

    Add the routes and controllers for posts and users

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   resources :users
-  get 'pages/WriteWave-Blog'
+  get '/users/:id', to: 'users#show'
+  get '/users/:user_id/posts', to: 'posts#index'
+  root 'home#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
