ce079d371ec6df31982c63673c90726e58fbe6ec
Robert Kanyoro <107240977+Kanyorok@users.noreply.github.com>
2023-10-28 18:00:20 +0300

    Fix overall layout and make the new comment route show the comment creation form

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,22 +1,22 @@
 class CommentsController < ApplicationController
   def new
-    @post = Post.find(params[:post_id])
     @comment = Comment.new
+    @post = Post.find(params[:post_id])
+    @user = current_user
   end
 
   def create
-    @comment = current_user.comments.build(comment_params.merge(post_id: params[:post_id]))
+    @user = current_user
+    @comment = @user.comments.build(comment_params)
+    @comment.post_id = params[:post_id]
 
     if @comment.save
-      flash[:success] = 'Added a new comment!'
+      flash[:success] = 'Comment created successfully!'
     else
-      flash[:error] = 'Comment was not added'
+      flash[:error] = 'Comment could not be saved.'
     end
-
-    redirect_to user_post_path(current_user, params[:post_id])
+    redirect_to user_post_path(current_user, @comment.post_id)
   end
-
-  private
 
   def comment_params
     params.require(:comment).permit(:text)
