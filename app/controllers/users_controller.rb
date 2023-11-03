1cbe9920070a5720f522940f005dac748add1990
Robert Kanyoro <107240977+Kanyorok@users.noreply.github.com>
2023-11-03 13:01:03 +0300

    Add the pages for devise config

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,4 +7,9 @@ class UsersController < ApplicationController
     @user = User.includes(posts: %i[comments author]).find(params[:id])
     @posts = @user.recentposts
   end
+
+  def sign_out_user
+    sign_out(current_user)
+    redirect_to root_path, notice: 'Signed out successfully'
+  end
 end
