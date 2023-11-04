81baa52404150175bbc30a6431ca44d9db627896
Robert Kanyoro <107240977+Kanyorok@users.noreply.github.com>
2023-11-04 18:00:58 +0300

    Add the config for managing users in user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,9 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  # :confirmable, :lockable, :timeoutable, :trackable, and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, :confirmable
+         :recoverable, :rememberable, :validatable
+
   has_many :comments, foreign_key: 'user_id'
   has_many :posts, foreign_key: 'author_id', primary_key: 'id'
   has_many :likes, foreign_key: 'user_id'
@@ -11,6 +12,13 @@ class User < ApplicationRecord
   validates :posts_count, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
   ROLES = %w[admin user].freeze
+
+  # Set a default role when creating a user
+  after_initialize :set_default_role, if: :new_record?
+
+  def set_default_role
+    self.role ||= 'user'
+  end
 
   def admin?
     role == 'admin'
